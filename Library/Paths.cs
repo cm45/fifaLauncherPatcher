c8e5e903c57b791a2a711d88d41afa2ffb03afbe
Christoph Müller <cmller49@gmail.com>
2020-04-01 20:49:19 +0200

    updated default path to fifa 20

--- Library/Paths.cs
+++ Library/Paths.cs
@@ -5,7 +5,7 @@ namespace Library
 {
     public static class Paths
     {
-        public static string DEFAULT_GAMEPATH = @"C:\Program Files (x86)\Origin Games\FIFA 19";
+        public static string DEFAULT_GAMEPATH = @"C:\Program Files (x86)\Origin Games\FIFA 20";
         public static string CONFIG_PATH = Path.Combine(Environment.CurrentDirectory, "config.json");
     }
 }
